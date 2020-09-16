e567b7634543532f2166eca2719b0eee87d2ac4c
mtodorov95 <mtodorov95@gmail.com>
2020-09-16 17:56:53 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Dashboard from "./components/layout/Dashboard";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tracks from "../api/Tracks";
+
+function Dashboard() {
+  return (
+    <div>
+      <Tracks />
+    </div>
+  );
+}
+
+export default Dashboard;
